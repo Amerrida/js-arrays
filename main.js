695f1fadb24ae3dc624f7e64f7d9a1af880083b4
Your NameAftan <aftan.merrida@gmail.com>
2018-08-28 19:26:00 -0500

    morearrays

--- main.js
+++ main.js
@@ -23,3 +23,28 @@ palChecker('mom');//yes
 palChecker('monkeybutt');//no
 palChecker('racecar');//yes
 console.log
+
+let animals = ['spiders', 'snakes','gophers','big spiders'];
+
+const lastItem = animals.pop();//big spiders
+const firstItem = animals.shift();//spiders
+animals.push('bat');
+animals.unshift('baby hippo');
+console.log('animals',animals);
+
+//Creating 4loops
+//1 what is your starting value—0 this will be 0 most of the time
+//2 what is your stopping value- something.length
+//3 how are you incrementing  +1
+//syntax is below
+for(starting;stoping; increment){
+//do something here
+}
+
+for(let i =0 ; i< animails.length; i++){
+    console.log(animails[i]);
+}
+]
+
+//this will 90% be the format the only thing you will have to change is animal
+
